4ddc44cc0713f806917722d0fbcae97c886b38e6
Mental Arena <caperaven@icloud.com>
2016-05-31 13:20:28 +0200

    first commit

---
+++ test/calculatorTests.ts
@@ -0,0 +1,11 @@
+import {expect} from 'chai'
+import {Calculator} from './../src/calculator';
+
+describe ("calculator tests", function() {
+   it("add", function() {
+        const calc = new Calculator();
+        const result = calc.add(2,3);
+        
+        expect(result).to.be.equal(5, '2 + 3 should be 5');
+   }) 
+});
